267dc503e733ba8d4d49e77dd6b2a24a7509d0c0
J. nathan Matias <natematias@gmail.com>
2011-12-28 21:41:03 -0500

    added passing test for getCsvWidth

--- AboveTheFold/src/AboveTheFoldTests/NYTDataSourceTest.java
+++ AboveTheFold/src/AboveTheFoldTests/NYTDataSourceTest.java
@@ -5,16 +5,20 @@ import java.util.*;
 import java.util.ArrayList;
 
 public class NYTDataSourceTest extends TestCase{
-  protected DataSource dataSource;
+  protected NYTDataSource dataSource;
   protected ArrayList dataStrings;
  
   public void setUp(){
     this.dataSource = new NYTDataSource();
-    this.dataStrings = ((NYTDataSource)this.dataSource).loadStrings("fixtures/nyt_data_source.csv");
+    this.dataStrings = this.dataSource.loadStrings("fixtures/nyt_data_source.csv");
   }
 
   public void testLoadStrings(){
     assertEquals(this.dataStrings.size(), 6);
+  }
+
+  public void testGetCsvWidth(){
+    assertEquals(4, this.dataSource.getCsvWidth(dataStrings));
   }
 
   private void printArrayList(ArrayList al){
