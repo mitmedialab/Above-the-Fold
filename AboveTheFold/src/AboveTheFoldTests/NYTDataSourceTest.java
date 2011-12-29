026731bb1e0f23862886222ac3e6314d28f9f209
J. nathan Matias <natematias@gmail.com>
2011-12-28 21:38:46 -0500

    refactored test to use setup

--- AboveTheFold/src/AboveTheFoldTests/NYTDataSourceTest.java
+++ AboveTheFold/src/AboveTheFoldTests/NYTDataSourceTest.java
@@ -5,12 +5,18 @@ import java.util.*;
 import java.util.ArrayList;
 
 public class NYTDataSourceTest extends TestCase{
+  protected DataSource dataSource;
+  protected ArrayList dataStrings;
+ 
+  public void setUp(){
+    this.dataSource = new NYTDataSource();
+    this.dataStrings = ((NYTDataSource)this.dataSource).loadStrings("fixtures/nyt_data_source.csv");
+  }
 
   public void testLoadStrings(){
-    DataSource dataSource = new NYTDataSource();
-    ArrayList strings = ((NYTDataSource)dataSource).loadStrings("fixtures/nyt_data_source.csv");
-    assertEquals(strings.size(), 6);
+    assertEquals(this.dataStrings.size(), 6);
   }
+
   private void printArrayList(ArrayList al){
     Iterator iterator = al.iterator();
     while(iterator.hasNext()){
